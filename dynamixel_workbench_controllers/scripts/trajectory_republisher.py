66935f157241f2b7098f435c3eb5d321d3f21775
Filip Zoric <fzoric8@gmail.com>
2023-04-25 13:54:38 +0200

    Added republishing of follow_joint_trajectory for the control of the real arm over moveit

--- dynamixel_workbench_controllers/scripts/trajectory_republisher.py
+++ dynamixel_workbench_controllers/scripts/trajectory_republisher.py
@@ -1,6 +1,7 @@
-
+#!/usr/bin/env python
 import rospy
 from trajectory_msgs.msg import JointTrajectory
+from control_msgs.msg import FollowJointTrajectoryActionGoal
 from std_msgs.msg import Bool
 
 class TrajectoryRepublisher():
@@ -9,17 +10,19 @@ class TrajectoryRepublisher():
 
         self.run_ready = False
         self.trajectory_reciv = False
-        self._init_publishers(); 
-        self._init_subscribers(); 
-        self.run_ready = True
 
         self.trajectory_sub_name = "arm_controller/follow_joint_trajectory/goal"
         self.trajectory_pub_name = "joint_trajectory"
         self.ready_sub_name = "ready"
 
+        self._init_publishers(); 
+        self._init_subscribers(); 
+        self.run_ready = True
+        rospy.loginfo("Initialized Trajectory Republisher")
+
     def _init_subscribers(self):
-        self.trajectory_sub = rospy.Subscriber(self.trajectory_sub_name, JointTrajectory, self.trajectory_cb)
-        self.ready_sub = rospy.Subscriber(self.ready_sub_name, Bool, self.ready_callback)
+        self.trajectory_sub = rospy.Subscriber(self.trajectory_sub_name, FollowJointTrajectoryActionGoal, self.trajectory_cb)
+        self.ready_sub = rospy.Subscriber(self.ready_sub_name, Bool, self.ready_cb)
 
     def _init_publishers(self): 
         self.trajectory_pub = rospy.Publisher(self.trajectory_pub_name, JointTrajectory, queue_size=1)
@@ -27,12 +30,17 @@ class TrajectoryRepublisher():
     def trajectory_cb(self, msg):
          
          self.trajectory_reciv = True
-         self.trajectory_msg = msg.goal.trajectory
+         self.trajectory_msg = JointTrajectory()
+         rospy.loginfo("Type of the trajectory is: {}".format(type(msg.goal.trajectory)))
+         self.trajectory_msg.points = msg.goal.trajectory.points
+         self.trajectory_msg.joint_names = msg.goal.trajectory.joint_names
+
          rospy.loginfo("Recieved trajectory: {}".format(self.trajectory_msg))
 
-    def ready_cb(self, msg):
+    def ready_cb(self, msg): 
          
          self.ready = msg.data
+         rospy.loginfo("Recieved ready: {}".format(self.ready))
 
     def run(self): 
 
